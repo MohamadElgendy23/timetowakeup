902bead992a1f9dc71fd4cad14950c8b097d0d8e
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-05-27 07:55:37 -0600

    utilizing cursor ai for development ! :)

--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-random',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './random.component.html',
   styleUrls: ['./random.component.css'],
 })
-export class RandomComponent {
-  randomCoffee: any;
+export class RandomComponent implements OnInit {
+  randomCoffee: string = '';
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getRandomCoffee().subscribe((randomCoffee) => {
-      this.randomCoffee = randomCoffee;
+    this.apiService.getRandomCoffee().subscribe((response: any) => {
+      this.randomCoffee = response.file;
     });
   }
 }
